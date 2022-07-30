496a4f79d5f41b1294122b090b4f9d04983b7744
Hasan-aga <careerforma@gmail.com>
2022-07-30 19:38:07 +0300

    implement showing a recipe

--- 18-forkify/starter/src/js/controller.js
+++ 18-forkify/starter/src/js/controller.js
@@ -104,19 +104,18 @@ const showRecipe = async function showRecipe() {
         <div class="recipe__ingredients">
           <h2 class="heading--2">Recipe ingredients</h2>
           <ul class="recipe__ingredient-list">
-            ${recipe.ingredients.map(ingredient => {
-              console.log(ingredient);
-              return `<li class="recipe__ingredient">
+            ${recipe.ingredients
+              .map(ingredient => {
+                return `<li class="recipe__ingredient">
               <svg class="recipe__icon">
-                <use href="src/img/icons.svg#icon-check"></use>
+                <use href="${icons}.svg#icon-check"></use>
               </svg>
-              <div class="recipe__quantity"></div>
               <div class="recipe__description">
-                <span class="recipe__unit"></span>
-                ${ingredient}
+              ${ingredient}
               </div>
             </li>`;
-            })}
+              })
+              .join('')}
 
            
           </ul>
@@ -126,17 +125,19 @@ const showRecipe = async function showRecipe() {
           <h2 class="heading--2">How to cook it</h2>
           <p class="recipe__directions-text">
             This recipe was carefully designed and tested by
-            <span class="recipe__publisher">The Pioneer Woman</span>. Please check out
+            <span class="recipe__publisher">${
+              recipe.publisher
+            }</span>. Please check out
             directions at their website.
           </p>
           <a
             class="btn--small recipe__btn"
-            href="http://thepioneerwoman.com/cooking/pasta-with-tomato-cream-sauce/"
+            href="${recipe.source_url}"
             target="_blank"
           >
             <span>Directions</span>
             <svg class="search__icon">
-              <use href="src/img/icons.svg#icon-arrow-right"></use>
+              <use href="${icons}.svg#icon-arrow-right"></use>
             </svg>
           </a>
         </div>
