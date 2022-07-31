4d90693f2d1176650fb5a3c0502f92f889397dc7
Hasan-aga <careerforma@gmail.com>
2022-07-31 17:37:51 +0300

    refactor api url to config module

--- 18-forkify/starter/src/js/model.js
+++ 18-forkify/starter/src/js/model.js
@@ -1,12 +1,12 @@
+import { API_URL } from './config';
+
 export const state = {
   recipe: {},
 };
 
 export const loadRecipe = async function (id) {
   try {
-    const res = await fetch(
-      `https://forkify-api.herokuapp.com/api/get?rId=${id}`
-    );
+    const res = await fetch(`${API_URL}${id}`);
     const data = await res.json();
     if (!res.ok) throw new Error(`faaail ${data.message}, ${res.status}`);
 
