32b10753b8fbbc194e0234c7dd9edcb86e395042
Hasan-aga <careerforma@gmail.com>
2022-07-31 20:33:24 +0300

    fix error message

--- 18-forkify/starter/src/js/helper.js
+++ 18-forkify/starter/src/js/helper.js
@@ -13,7 +13,7 @@ export const getJson = async function (url) {
     const res = await Promise.race([fetch(url), timeout(TIMEOUT_SECONDS)]);
     const data = await res.json();
     if (!res.ok)
-      throw new Error(`failed to get data: ${data.message}, ${res.status}`);
+      throw new Error(`failed to get data: ${data.error}, ${res.status}`);
     return data;
   } catch (error) {
     throw error;
