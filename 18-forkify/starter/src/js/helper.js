a0add9cdc73c328fcd32b76db6571e679d18b934
Hasan-aga <careerforma@gmail.com>
2022-07-31 17:57:43 +0300

    create timeout for promise termination

--- 18-forkify/starter/src/js/helper.js
+++ 18-forkify/starter/src/js/helper.js
@@ -1,6 +1,14 @@
+const timeout = function (s) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error(`Request took too long! Timeout after ${s} second`));
+    }, s * 1000);
+  });
+};
+
 export const getJson = async function (url) {
   try {
-    const res = await fetch(url);
+    const res = await Promise.race([fetch(url), timeout(5)]);
     const data = await res.json();
     if (!res.ok)
       throw new Error(`failed to get data: ${data.message}, ${res.status}`);
