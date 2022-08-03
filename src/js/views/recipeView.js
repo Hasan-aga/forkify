543e0e0fe6359a760e25f3d8f0c0bc1dd1ccd6eb
Hasan-aga <careerforma@gmail.com>
2022-08-04 00:47:04 +0300

    rebuild

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -11,7 +11,7 @@ class RecipeView extends View {
   _successMessage = "";
 
   addHandlerRender(handler) {
-    console.log("icons", icons);
+    console.log("icons:", icons);
     ["hashchange", "load"].forEach((ev) =>
       window.addEventListener(ev, handler)
     );
