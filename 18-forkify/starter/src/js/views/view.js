c0a71f95dd72999cd6cecfc12953e596413b7441
Hasan-aga <careerforma@gmail.com>
2022-08-03 17:04:00 +0300

    fix view bug

--- 18-forkify/starter/src/js/views/view.js
+++ 18-forkify/starter/src/js/views/view.js
@@ -2,7 +2,7 @@
 import icons from 'url:../../img/icons.svg';
 
 export default class View {
-  _parentElement = document.querySelector('.recipe');
+  _parentElement;
   _data;
 
   renderSpinner() {
