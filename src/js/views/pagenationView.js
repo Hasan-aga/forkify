6f700f741bc2950e0d341f50c704d14491c441b5
Hasan-aga <careerforma@gmail.com>
2022-08-03 22:04:53 +0300

    return to using url for icon imports

--- src/js/views/pagenationView.js
+++ src/js/views/pagenationView.js
@@ -1,6 +1,6 @@
 "use strict";
 import View from "./view";
-import icons from "../../img/icons.svg";
+import icons from "url:../../img/icons.svg";
 
 class PaginationView extends View {
   _parentElement = document.querySelector(".pagination");
